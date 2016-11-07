8b34bf828aa5b358b73d61d0680b731f9eb97225
Marcin Pilaczynski <marcin.pil@gmail.com>
2016-11-07 20:51:19 +0100

    Introduce @OneToMany

--- src/main/java/com/pluralsight/model/Exercise.java
+++ src/main/java/com/pluralsight/model/Exercise.java
@@ -1,17 +1,36 @@
 package com.pluralsight.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Range;
 
-
+@Entity
 public class Exercise {
-	
+
+	@Id
+	@GeneratedValue
+	Long id;
+
 	@Range(min = 1, max = 120)
 	private int minutes;
-	
+
 	@NotNull
 	private String activity;
+
+	@ManyToOne
+	private Goal goal;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getActivity() {
 		return activity;
--- src/main/java/com/pluralsight/model/Goal.java
+++ src/main/java/com/pluralsight/model/Goal.java
@@ -3,6 +3,8 @@ package com.pluralsight.model;
 import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -17,6 +19,9 @@ public class Goal {
     @Range(min = 1, max = 120)
     @Column(name = "MINUTES")
     private int minutes;
+
+    @OneToMany(mappedBy = "goal", cascade = CascadeType.ALL)
+    private List<Exercise> exercises = new ArrayList<Exercise>();
 
     public Long getId() {
         return id;
