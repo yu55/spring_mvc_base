ced8922f0106c5654e54392da8db416d1017d2a9
Marcin Pilaczynski <marcin.pil@gmail.com>
2016-11-14 21:19:53 +0100

    ExerciseRepository via spring-data-jpa

--- src/main/java/com/pluralsight/repository/ExerciseRepository.java
+++ src/main/java/com/pluralsight/repository/ExerciseRepository.java
@@ -1,8 +1,9 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Exercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ExerciseRepository {
-
-    Exercise save(Exercise exercise);
+@Repository("exerciseRepository")
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 }
